b4fe1605facae97dd84249132dd7fc818d2bf28a
Thanos Alexopoulos <talexop@gmail.com>
2014-03-05 18:53:48 +0200

    first commit kernel v2+

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 3 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#3 SMP PREEMPT Fri Feb 28 11:36:12 EET 2014"
+#define LINUX_COMPILE_BY "root"
+#define LINUX_COMPILE_HOST "talexop-linux"
+#define LINUX_COMPILER "gcc version 4.6.x-google 20120106 (prerelease) (GCC) "
